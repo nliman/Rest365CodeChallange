0649daabb211c08ac1add3e0ca31e1d43cb4fdbe
nliman <nawa.sari@gmail.com>
2020-01-10 14:55:42 -0800

    Step2
    Step2 - Remove the maximum constraint for numbers e.g. 1,2,3,4,5,6,7,8,9,10,11,12 will return 78

---
+++ StringCalculator/Step2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculator
+{
+    public class Step2 : Step1, IStringCalculator
+    {
+        /// <summary>
+        /// Method to Validate string's length and format, without maximum constraint
+        /// </summary>
+        /// <param name="stringToProcess">Provide string to be validated</param>
+        /// <returns>Array of validated numbers</returns>
+        public override string[] ValidateString(string stringToValidate)
+        {
+            try
+            {
+                char[] seperators = { ',', ' ', };
+                string[] numbers = stringToValidate.Split(seperators, StringSplitOptions.RemoveEmptyEntries);
+
+                return numbers;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ StringCalculatorTests/Step2.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculatorTests
+{
+    [TestClass]
+    public class Step2 : Step1, IStringCalculator
+    {
+        [TestMethod]
+        public void FormatString_NoLimit()
+        {
+            Assert.AreEqual("12", ValidateString("1,2,3,4,5,6,7,8,9,10,11,12").Length.ToString());
+        }
+
+        public override void StringValidation_MoreThanTwo()
+        {
+            string[] returnValue = ValidateString("123,456,789");
+            Assert.AreEqual("123", returnValue[0].ToString());
+            Assert.AreEqual("456", returnValue[1].ToString());
+            Assert.AreEqual("789", returnValue[2].ToString());
+        }
+
+        public override string[] ValidateString(string stringToValidate)
+        {
+            char[] seperators = { ',', ' ', };
+            string[] numbers = stringToValidate.Split(seperators, StringSplitOptions.RemoveEmptyEntries);
+            return numbers;
+        }
+    }
+}
