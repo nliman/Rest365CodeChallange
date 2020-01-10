0d29534e9abaee33a759d28531f36bdab7816abe
nliman <nawa.sari@gmail.com>
2020-01-10 15:01:31 -0800

    Step 5
    Step 5 Commit - Make any value greater than 1000 an invalid number e.g. 2,1001,6 will return 8

---
+++ StringCalculator/Step5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculator
+{
+    public class Step5 : Step4, IStringCalculator
+    {
+        /// <summary>
+        /// Method to add all numbers provided in a list, exclude any number that's greater than 1000
+        /// </summary>
+        /// <param name="numbers">Collection of numbers to be added</param>
+        /// <returns>Total of provided numbers </returns>
+        public override int AddNumbers(List<string> numbers)
+        {
+            int returnValue = 0;
+
+            foreach (var number in numbers)
+            {
+                if (Convert.ToInt32(number) <= 1000)
+                {
+                    returnValue += Convert.ToInt32(number);
+                }
+            }
+
+            return returnValue;
+        }
+    }
+}
---
+++ StringCalculatorTests/Step5.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using StringCalculator;
+
+namespace StringCalculatorTests
+{
+    [TestClass]
+    public class Step5: Step4, IStringCalculator
+    {
+        [TestMethod]
+        public void NumberSummation_HasLargeNumber()
+        {
+            List<string> numbers = new List<string>();
+            numbers.Add("2");
+            numbers.Add("1001");
+            numbers.Add("6");
+            Assert.AreEqual("8", AddNumbers(numbers).ToString());
+        }
+        public override int AddNumbers(List<string> numbers)
+        {
+            int returnValue = 0;
+
+            foreach (var number in numbers)
+            {
+                if (Convert.ToInt32(number) <= 1000)
+                {
+                    returnValue += Convert.ToInt32(number);
+                }
+            }
+
+            return returnValue;
+        }
+    }
+}
