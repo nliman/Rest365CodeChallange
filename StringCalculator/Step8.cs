a584548b3401903e38aadfc911338acf7e5e67f9
nliman <nawa.sari@gmail.com>
2020-01-10 15:05:35 -0800

    Step 8
    Step 8 Commit - Support multiple delimiters of any length using the format: //[{delimiter1}][{delimiter2}]...\n{numbers}
      - example: //[*][!!][r9r]\n11r9r22*hh*33!!44 will return 110
      - all previous formats should also be supported

---
+++ StringCalculator/Step8.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculator
+{
+    public class Step8: Step7, IStringCalculator
+    {
+        /// <summary>
+        /// Method to Validate string's length and format, supports 1 custom delimiter and delimiter with any length
+        /// </summary>
+        /// <param name="stringToProcess">Provide string to be validated</param>
+        /// <param name="customDelims">String Array of custom delimiters</param>
+        /// <returns>Array of validated numbers</returns>
+        public string[] ValidateString(string stringToValidate, string[] customDelims)
+        {
+            string[] numbers;
+
+            try
+            {
+                numbers = stringToValidate.Split(customDelims, StringSplitOptions.RemoveEmptyEntries);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return numbers;
+        }
+    }
+}
---
+++ StringCalculatorTests/Step8.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using StringCalculator;
+
+namespace StringCalculatorTests
+{
+    [TestClass]
+    public class Step8 : Step7, IStringCalculator
+    {
+        [TestMethod]
+        public override void StringValidation_CustomDelims()
+        {
+            char[] seperators = new char[] { ',', ' ', '\n' };
+            string[] customDelims = new string[] { "[*]", "[!!]", "[r9r]", "r9r", "*hh*", "!!" };
+
+            //char[][] char2dArray = new char[customDelims.Length + seperators.Length][];
+            //int counter = 0;
+            //for (int i = 0; i < seperators.Length; i++)
+            //{
+            //    char2dArray[i] = seperators[i].ToString().ToCharArray();
+            //    counter = i;
+            //}
+            //counter++;
+            //for (int i = 0; i < customDelims.Length; i++)
+            //{
+            //    char2dArray[counter] = customDelims[i].ToCharArray();
+            //    counter++;
+            //}
+
+            string[] delims = new string[seperators.Length + customDelims.Length];
+
+            Array.Copy(customDelims, delims, customDelims.Length);
+            int counter = customDelims.Length;
+            for (int i = 0; i < seperators.Length; i++)
+            {
+                delims[counter] = seperators[i].ToString();
+                counter++;
+            }
+
+            string[] numbers = ValidateString(("//[*][!!][r9r]\n11r9r22*hh*33!!44"), delims);
+            foreach (var number in numbers)
+            {
+                Assert.AreEqual("//", numbers[0]);
+                Assert.AreEqual("11", numbers[1]);
+                Assert.AreEqual("22", numbers[2]);
+                Assert.AreEqual("33", numbers[3]);
+                Assert.AreEqual("44", numbers[4]);
+            }
+        }
+
+        public string[] ValidateString(string stringToValidate, string[] customDelims)
+        {
+            string[] numbers = stringToValidate.Split(customDelims, StringSplitOptions.RemoveEmptyEntries);
+            return numbers;
+        }
+    }
+}
