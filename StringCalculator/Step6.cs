51a2634692b3dbafedcba26166ea775580600b7b
nliman <nawa.sari@gmail.com>
2020-01-10 15:02:58 -0800

    Step 6
    Step 6 Commit - Support 1 custom delimiter of a single character using the format: //{delimiter}\n{numbers}
      - examples: //#\n2#5 will return 7; //,\n2,ff,100 will return 102
      - all previous formats should also be supported

---
+++ StringCalculator/Step6.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculator
+{
+    public class Step6 : Step5, IStringCalculator
+    {
+        /// <summary>
+        /// Method to Validate string's length and format, supports 1 custom delimiter and delimiter with any length
+        /// </summary>
+        /// <param name="stringToProcess">Provide string to be validated</param>
+        /// <param name="customDelims">Char Array of custom delimiters</param>
+        /// <returns>Array of validated numbers</returns>
+        public string[] ValidateString(string stringToValidate, char[] customDelims)
+        {
+            string[] numbers;
+
+            try
+            {
+                char[] seperators = new char[] { ',', ' ', '\n' };
+                char[] delims = new char[seperators.Length + customDelims.Length];
+
+                var byteIndex = seperators.Length * sizeof(char);
+                Buffer.BlockCopy(seperators, 0, delims, 0, byteIndex);
+                Buffer.BlockCopy(customDelims, 0, delims, byteIndex, customDelims.Length * sizeof(char));
+
+                numbers = stringToValidate.Split(delims, StringSplitOptions.RemoveEmptyEntries);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return numbers;
+        }
+    }
+}
---
+++ StringCalculatorTests/Step6.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using StringCalculator;
+
+namespace StringCalculatorTests
+{
+    [TestClass]
+    public class Step6 : Step5, IStringCalculator
+    {
+        [TestMethod]
+        public virtual void StringValidation_CustomDelims()
+        {
+            char[] customDelims = { '#' };
+            string[] numbers = ValidateString("//#\n2,3#5", customDelims);
+            foreach (var number in numbers)
+            {
+                Assert.AreEqual("//", numbers[0]);
+                Assert.AreEqual("2", numbers[1]);
+                Assert.AreEqual("3", numbers[2]);
+                Assert.AreEqual("5", numbers[3]);
+            }
+        }
+
+        public string[] ValidateString(string stringToValidate, char[] customDelims)
+        {
+            char[] seperators = new char[] { ',', ' ', '\n' };
+
+            char[] delims = new char[seperators.Length + customDelims.Length];
+
+            var byteIndex = seperators.Length * sizeof(char);
+            Buffer.BlockCopy(seperators, 0, delims, 0, byteIndex);
+            Buffer.BlockCopy(customDelims, 0, delims, byteIndex, customDelims.Length * sizeof(char));
+            
+            string[] numbers = stringToValidate.Split(delims, StringSplitOptions.RemoveEmptyEntries);
+            return numbers;
+        }
+    }
+}
