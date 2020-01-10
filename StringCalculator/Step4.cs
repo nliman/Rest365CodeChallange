6743133bba17df4fe6ab484dc0bb1adf83227449
nliman <nawa.sari@gmail.com>
2020-01-10 15:00:07 -0800

    Step 4
    Step 4 Commit - Deny negative numbers by throwing an exception that includes all of the negative numbers provided

---
+++ StringCalculator/Step4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculator
+{
+    public class Step4 : Step3, IStringCalculator
+    {
+        /// <summary>
+        /// Method to format numbers, throws error when any negative number is detected
+        /// </summary>
+        /// <param name="stringToProcess">string to be formatted</param>
+        /// <returns>Formatted number</returns>
+        public override string FormatNumber(string stringToProcess)
+        {
+            string number = string.Empty;
+
+            try
+            {
+                char[] chars = stringToProcess.ToCharArray();
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    if (chars[i] == 45)
+                    {
+                        throw new Exception("Negative number is not allowed");
+                    }
+                    else if (chars[i] >= 48 && chars[i] <= 57)
+                    {
+                        number += chars[i].ToString();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return number;
+        }
+    }
+}
---
+++ StringCalculatorTests/Step4.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using StringCalculator;
+
+namespace StringCalculatorTests
+{
+    [TestClass]
+    public class Step4 : Step3, IStringCalculator
+    {
+        [TestMethod]
+        public override void CheckFormat_NegativeNumber()
+        {
+            try
+            {
+                FormatNumber("-1");
+            }
+            catch (Exception)
+            {
+                return;
+            }
+            Assert.Fail();
+        }
+
+        public override string FormatNumber(string stringToProcess)
+        {
+            string number = string.Empty;
+            char[] chars = stringToProcess.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == 45 )
+                {
+                    throw new Exception("Negative number is not allowed");
+                }
+                else if (chars[i] >= 48 && chars[i] <= 57)
+                {
+                    number += chars[i].ToString();
+                }
+            }
+
+            return number;
+        }
+    }
+}
