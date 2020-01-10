fdcb6be70b1c737fc19cdf4698eb775af7e48db4
nliman <nawa.sari@gmail.com>
2020-01-10 14:57:57 -0800

    Step3
    Step 3 Commit - Support a newline character as an alternative delimiter e.g. 1\n2,3 will return 6

---
+++ StringCalculator/Step3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculator
+{
+    public class Step3 : Step2, IStringCalculator
+    {
+        /// <summary>
+        /// Method to Validate string's length and format, supports newline character as new delimiter
+        /// </summary>
+        /// <param name="stringToProcess">Provide string to be validated</param>
+        /// <returns>Array of validated numbers</returns>
+        public override string[] ValidateString(string stringToValidate)
+        {
+            string[] numbers;
+
+            try
+            {
+                char[] seperators = { ',', ' ', '\n' };
+                numbers = stringToValidate.Split(seperators, StringSplitOptions.RemoveEmptyEntries);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return numbers;
+        }
+    }
+}
---
+++ StringCalculatorTests/Step3.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using StringCalculator;
+
+namespace StringCalculatorTests
+{
+    [TestClass]
+    public class Step3 : Step2, IStringCalculator
+    {
+        [TestMethod]
+        public void CheckString_NonCommaDelimiter()
+        {
+            Assert.AreEqual(3, ValidateString("1\n2,3").Length);
+        }
+
+        public override string[] ValidateString(string stringToValidate)
+        {
+            char[] seperators = { ',', ' ', '\n' };
+            string[] numbers = stringToValidate.Split(seperators, StringSplitOptions.RemoveEmptyEntries);
+            return numbers;
+        }
+    }
+}
