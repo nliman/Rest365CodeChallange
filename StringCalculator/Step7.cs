5ff888d94862b2cc46130d13d81b4d3f5824bb9a
nliman <nawa.sari@gmail.com>
2020-01-10 15:04:13 -0800

    Step 7
    Step 7 Commit - Support 1 custom delimiter of any length using the format: //[{delimiter}]\n{numbers}
      - example: //[***]\n11***22***33 will return 66
      - all previous formats should also be supported

---
+++ StringCalculator/Step7.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculator
+{
+    public class Step7 : Step6, IStringCalculator
+    {
+        //no code changes as step 6 handles both single and delimiter of any length scenarios
+    }
+}
---
+++ StringCalculatorTests/Step7.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using StringCalculator;
+
+namespace StringCalculatorTests
+{
+    [TestClass]
+    public class Step7 : Step6, IStringCalculator
+    {
+        [TestMethod]
+        public override void StringValidation_CustomDelims()
+        {
+            char[] customDelims = ("[***]").ToCharArray();
+
+            string[] numbers = ValidateString("//[***]\n11***22***33", customDelims);
+            foreach (var number in numbers)
+            {
+                Assert.AreEqual("//", numbers[0]);
+                Assert.AreEqual("11", numbers[1]);
+                Assert.AreEqual("22", numbers[2]);
+                Assert.AreEqual("33", numbers[3]);
+            }
+        }
+    }
+}
